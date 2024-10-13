04adebbf5f784c930aea5976bc4a0963e549820c
kkawayuki <kyukiokawa@gmail.com>
2024-10-13 00:37:27 -0700

    Commit 7

--- lab21.cpp
+++ lab21.cpp
@@ -16,8 +16,8 @@ public:
     Goat() // default constructor
     {
         age = (rand() % 20) + 1;     // random variable (1-20)
-        name = names[rand() % 15];   // random member of array of names
-        color = colors[rand() % 15]; // random member of array of colors
+        name = names[rand() % 15];   // random member from array of names
+        color = colors[rand() % 15]; // random member frpm array of colors
     }
 
     Goat(int a, string n, string c) // full parameter constructor
@@ -27,7 +27,14 @@ public:
         color = c;
     }
 
-    void printGoatFields() const //get it? Goat Fields? Do goats graze in fields? Are goats herbivores? not too sure on this one but probably still a pun. 
+    /************************************************
+     * Function: Prints out all goat fields with 
+     * a tab for formatting. 
+     *
+     * Parameters: NONE
+     * Return: NONE
+     ************************************************/
+    void printGoatFields() const //insert Goat field pun
     {
         cout << '\t' << name << " (" << color << ", " << age << ")\n";
     }
@@ -37,12 +44,12 @@ private:
     string name;
     string color;
 
-    const string names[ARRSIZE] = { // 15 uncharacteristically human names
+    const string names[ARRSIZE] = { // array of 15 strangely human names
         "John", "Michael", "Sarah", "Emily", "David",
         "Jessica", "James", "Jennifer", "Robert", "Laura",
         "Christopher", "Ashley", "Matthew", "Amanda", "Daniel"};
 
-    const string colors[ARRSIZE] = {
+    const string colors[ARRSIZE] = { // array of 15 AI-generated oddly specific colors
         "White", "Black", "Brown", "Gray", "Golden",
         "Spotted", "Cream", "Beige", "Tan", "Chocolate",
         "Silver", "Ash", "Ivory", "Ebony", "Mahogany"};
@@ -51,17 +58,15 @@ private:
 // class: DoublyLinkedList
 class DoublyLinkedList
 {
-friend Goat; //so that DoublyLinkedList can access Goat methods
-
 private:
     struct Node
     {
-        Goat g; //each member in linked list has a goat
+        Goat g; // each Node in linked list has a goat!
         Node *prev;
         Node *next;
 
-        Node(const Goat &goat ,Node* p = nullptr, Node* n = nullptr) //constructor, takes in goat as parameter
-        : g(goat), prev(p), next(n) // brackets don't work for mine for some reason
+        Node(const Goat &goat, Node *p = nullptr, Node *n = nullptr) // constructor, takes in goat as parameter
+            : g(goat), prev(p), next(n)                              // brackets don't work for mine for some reason, g(goat) uses the copy constructor to fully assign relevant fields.
         {}
     };
 
@@ -76,7 +81,7 @@ public:
         tail = nullptr;
     }
 
-    void push_back(const Goat &goat) //modified to push back goats
+    void push_back(const Goat &goat) 
     {
         Node *newNode = new Node(goat);
         if (!tail) // if there's no tail, the list is empty
@@ -102,7 +107,7 @@ public:
         }
     }
 
-    /* temporarily disable for ease of use
+    /* disabled for ease of use (I didn't have to modify these)
 
     void insert_after(int value, int position)
     {
@@ -178,14 +183,14 @@ public:
         Node *current = head;
         if (!current)
         {
-            cout << "empty";
+            cout << "List is empty.";
             return;
         }
-            
+
         while (current)
         {
-            current->g.printGoatFields(); //print all information about goat
-            current = current->next; //move to next
+            current->g.printGoatFields(); // print all information about goat
+            current = current->next;      // move to next
         }
         cout << endl;
     }
@@ -194,16 +199,20 @@ public:
     {
         Node *current = tail;
         if (!current)
+        {
+            cout << "List is empty.";
             return;
+        }
         while (current)
         {
-            current->g.printGoatFields(); //same deal in reverse order
-            current = current->prev; //go previous
+            current->g.printGoatFields(); // same deal in reverse order
+            current = current->prev;      // go previous
         }
         cout << endl;
     }
 
-    ~DoublyLinkedList()
+    //destructor
+    ~DoublyLinkedList() 
     {
         while (head)
         {
@@ -217,19 +226,21 @@ public:
 // Driver program
 int main()
 {
-    srand(time(0)); // seed time-dependent random
+    // seed time-dependent random variable
+    srand(time(0)); 
 
     // variable initializaition
-    DoublyLinkedList list;
     int size = (rand() % 15) + 5; // size of array random (5-20)
+    DoublyLinkedList list;
 
-    //assignment loop
+    // assignment loop
     for (int i = 0; i < size; i++)
     {
-        Goat temp;
-        list.push_back(temp); // see how this works
+        Goat temp; //create a temp goat
+        list.push_back(temp); //push back the goat by reference
     }
 
+    //Output manipulation with full list
     cout << "Forward: \n";
     list.print();
 
